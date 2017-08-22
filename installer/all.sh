4455a7a76071739ead062ec1889adf2b5936099a
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2017-08-23 06:02:18 +0900

    Update installer and utils scripts
    * Add including for utils/wf.sh into .bashrc
    
    * Remove nouse lines
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- installer/all.sh
+++ installer/all.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 BASE_DIR=/vagrant/installer
+UTILS_DIR=/vagrant/utils
 
 echo "Install devstack and related packages"
 source ${BASE_DIR}/devstack.sh
 
 echo "Install dein for management vim packages"
 source ${BASE_DIR}/dein.sh
-cp -i ${BASE_DIR}/vimrc_sample.txt $HOME/.vimrc
+cp -i ${BASE_DIR}/vimrc_sample.txt ${HOME}/.vimrc
+
+cat "source ${UTILS_DIR}/wf.sh" >> ${HOME}/.bashrc
 
 echo "Done!"
--- utils/wf.sh
+++ utils/wf.sh
@@ -1,30 +1,3 @@
-# switch proxy env
-proxy() {
-  if [ ! -z ${http_proxy} ]; then
-    MY_HTTP_PROXY=${http_proxy}
-  fi
-  if [ ! -z ${https_proxy} ]; then
-    MY_HTTPS_PROXY=${https_proxy}
-  fi
-  if [ ! -z ${no_proxy} ]; then
-    MY_NO_PROXY=${no_proxy}
-  fi
-
-  if [ $# = 0 ]; then
-    echo "Usage: proxy on|off"
-    echo ""
-    env | grep proxy
-  elif [ $1 == "on" ]; then
-    export http_proxy=${MY_HTTP_PROXY}
-    export https_proxy=${MY_HTTPS_PROXY}
-    export no_proxy=${MY_NO_PROXY}
-  elif [ $1 == "off" ]; then
-    export http_proxy=
-    export https_proxy=
-    export no_proxy=
-  fi
-}
-
 # word find
 wf () {
   HELP_MSG="Usage: wf [-i] [-c] [-d path] word"

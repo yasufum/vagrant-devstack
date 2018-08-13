11eccac74b1346cae847e65b759d2c6c285c64bc
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2018-08-13 17:57:56 +0900

    Add jedi installation
    jedi-vim is a useful tool for python developer.
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- installer/nvim.sh
+++ installer/nvim.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# install nvim
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update -y
 sudo apt-get install neovim -y
+
 sudo apt-get install python3-dev python3-pip -y
+sudo pip install neovim
+sudo pip3 install neovim
+
+sudo pip install jedi
+sudo pip3 install jedi
 
 pip3 install -U pip3

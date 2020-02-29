c8e72839ae5e7cf7bcce75be0bf29ad1d2f66674
Yasufumi Ogawa <yasufum.o@gmail.com>
2020-02-29 12:06:09 +0900

    Add core.eidtor in gitconfig generator
    Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

--- helper/git_setup_gen.sh
+++ helper/git_setup_gen.sh
@@ -7,4 +7,5 @@ echo "" >> ${TARGET}
 echo git config --global user.name \"$(git config --get user.name)\" >>  ${TARGET}
 echo git config --global user.email \"$(git config --get user.email)\" >> ${TARGET}
 echo git config --global gitreview.username \"$(git config --get gitreview.username)\" >> ${TARGET}
+echo git config --global core.editor \"$(git config --get core.editor)\" >> ${TARGET}
 chmod 775 ${TARGET}

6398225eb39823ef1f78a4466ac1b9977d6e460a
Yasufumi Ogawa <yasufum.o@gmail.com>
2020-08-17 18:09:24 +0900

    Install wf command from git repo
    `wf.sh` is provided as a part of this installer although original one is
    hosted on [1]. So, it should be updated every time if original one is
    updated. It can be a pain for maintenance. This update is to change to
    install clone original `wf.sh` while installation.
    
    Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

--- installer/all.sh
+++ installer/all.sh
@@ -6,10 +6,10 @@ BASE_DIR=/vagrant/installer
 UTIL_DIR=/vagrant/util
 
 echo "PATH=$PATH:/sbin" >> ${HOME}/.bashrc
-echo "source ${UTIL_DIR}/wf.sh" >> ${HOME}/.bashrc
 
 echo "Install docker"
 source ${BASE_DIR}/docker.sh
+
 
 echo "Install vim8, vim-plug and lang servers"
 source ${BASE_DIR}/vim8.sh
@@ -20,9 +20,14 @@ source ${BASE_DIR}/lang-servers.sh
 #source ${BASE_DIR}/nvim.sh
 #source ${BASE_DIR}/dein.sh
 
+
 echo "Install devstack and related packages"
 source ${BASE_DIR}/devstack.sh
 
+
+echo "Install optional tools"
 source ${BASE_DIR}/ops-tools.sh
+git clone https://github.com/yasufum/bash_utils.git
+echo "source ${HOME}/bash_utils/wf.sh" >> ${HOME}/.bashrc
 
 echo "Done!"

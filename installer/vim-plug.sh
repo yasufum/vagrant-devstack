c712a743f122a6bf070736790b2938c4fb8ad8a9
Yasufumi Ogawa <yasufum.o@gmail.com>
2020-09-04 14:26:42 +0900

    Add ctags installation
    To enable taglist plugin, add `apt install exuberant-ctags`.
    
    Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

--- installer/vim-plug.sh
+++ installer/vim-plug.sh
@@ -6,3 +6,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cp -i ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc
+
+# Ctags is required for plugin 'vim-scripts/taglist.vim'
+sudo apt install exuberant-ctags -y

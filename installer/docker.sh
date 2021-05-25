2b27fabdce7e326232179a6962ee1245e614aa14
Yasufumi Ogawa <yasufum.o@gmail.com>
2021-05-26 02:38:14 +0900

    Remove old installer scripts
    Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

--- installer/docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install -y docker.io
-
-sudo groupadd docker
-sudo usermod -aG docker $USER
--- installer/extras.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install jq -y
-sudo apt-get install nmon -y  # resource monitor
-sudo apt-get install lnav -y  # log viewer
-
-sudo apt-get install firefox -y  # useful if you enable X11 forwarding
